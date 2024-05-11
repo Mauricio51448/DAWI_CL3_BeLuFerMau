0281783eaaf39e628190b57cefbc579ceee69ff9
Mauricio51448 <mvmauriciomiki@gmail.com>
2024-05-11 12:37:20 -0500

    añadiendo funcionalidad al componente dni

--- src/app/form-calcular-aprobacion-dni/form-calcular-aprobacion-dni.component.ts
+++ src/app/form-calcular-aprobacion-dni/form-calcular-aprobacion-dni.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-calcular-aprobacion-dni',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-calcular-aprobacion-dni.component.html',
   styleUrl: './form-calcular-aprobacion-dni.component.scss'
 })
 export class FormCalcularAprobacionDniComponent {
+
+  yearOfBirth: number;
+  dniResult: { type: string, message: string } | null;
+
+  constructor() {
+    this.yearOfBirth = 0;
+    this.dniResult = null;
+  }
+
+  onSubmit() {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - this.yearOfBirth;
+
+    if (age >= 18) {
+      this.dniResult = { type: 'success', message: 'Aprobado para DNI Azul' };
+    } else {
+      this.dniResult = { type: 'danger', message: 'Aún no puede sacar su DNI' };
+    }
+  }
+
 
 }

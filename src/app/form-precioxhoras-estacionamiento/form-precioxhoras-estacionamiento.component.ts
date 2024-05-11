f98d3b554511631455622d9712efb3bcac22e44b
LuisRoj <luisrojas150520@gmail.com>
2024-05-11 10:36:34 -0500

    Inicializacion de variables y enrutamiento de estacionamiento

--- src/app/form-precioxhoras-estacionamiento/form-precioxhoras-estacionamiento.component.ts
+++ src/app/form-precioxhoras-estacionamiento/form-precioxhoras-estacionamiento.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-precioxhoras-estacionamiento',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-precioxhoras-estacionamiento.component.html',
   styleUrl: './form-precioxhoras-estacionamiento.component.scss'
 })
 export class FormPrecioxhorasEstacionamientoComponent {
 
+  horas: number = 0;
+  minutos: number = 0;
+  costo: number = 0;
+
+  calcularCosto(): void {
+    let totalHoras = this.horas + (this.minutos > 0 ? 1 : 0);
+    this.costo = totalHoras * 1.5;
+  }
 }
+

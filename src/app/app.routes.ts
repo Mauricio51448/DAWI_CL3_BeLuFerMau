2250f6eec804cdefd2655a8ba1e4e128439fbdff
Fernando <monasoxx@gmail.com>
2024-05-11 00:32:51 -0500

    comenzando cl3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FormMostrarParesRangoComponent } from './form-mostrar-pares-rango/form-mostrar-pares-rango.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:FormMostrarParesRangoComponent}
+];
